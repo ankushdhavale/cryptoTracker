e9fb3c64e9607dcbd28d8f20323c133a2fe64d2c
Ankush Kailas Dhavale <dhavaleankush7@gmail.com>
2025-05-05 23:18:12 +0530

    changes

--- src/pages/CoinPage.jsx
+++ src/pages/CoinPage.jsx
@@ -4,7 +4,8 @@ import Loader from "../components/Common/Loader/Loader";
 import axios from "axios";
 import { coinObject } from "../functions/convertObject";
 import List from "../components/Dashboard/List/List";
-import "../index.css"
+import "../index.css";
+import CoinInfo from "../components/Coin/CoinInfo/CoinInfo";
 
 const CoinPage = () => {
 	const [isLoading, setIsLoading] = useState(true);
@@ -24,6 +25,13 @@ const CoinPage = () => {
 					console.log("Error", error);
 					setIsLoading(true);
 				});
+
+			axios.get(
+				`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7&interval=daily`
+			).then((response) => {
+				console.log("Response>>>>>>",response.data.prices);
+				
+			})
 		}
 	}, [coinId]);
 
@@ -32,9 +40,12 @@ const CoinPage = () => {
 			{isLoading ? (
 				<Loader />
 			) : (
-				<div className="grey-wrapper">
-                        <List coin={coinData} />
-				</div>
+				<>
+					<div className='grey-wrapper'>
+						<List coin={coinData} />
+					</div>
+					<CoinInfo heading={coinData.name} desc={coinData.desc} />
+				</>
 			)}
 		</div>
 	);
