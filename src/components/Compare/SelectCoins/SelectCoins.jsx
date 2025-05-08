b0749aa23f8f42fafcda6773dfb522ea1524974f
Ankush Kailas Dhavale <dhavaleankush7@gmail.com>
2025-05-08 16:39:01 +0530

    listPage responsive

--- src/components/Compare/SelectCoins/SelectCoins.jsx
+++ src/components/Compare/SelectCoins/SelectCoins.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const SelectCoins = () => {
-  return (
-    <div>
-      hello
-    </div>
-  )
-}
+	const styles = {
+		height: "2.5rem",
+		color: "var(--white)",
+		"& .MuiOutlinedInput-notchedOutline": {
+			borderColor: "var(--white)",
+		},
+		"& .MuiSvgIcon-root": {
+			color: "var(--white)",
+		},
+		"&:hover": {
+			"&& fieldset": {
+				borderColor: "#3a80e9",
+			},
+		},
+	};
+	return (
+		<div>
+			<Select
+				sx={styles}
+				value={days}
+				label='Days'
+				onChange={handleDaysChange}
+			>
+				<MenuItem value={7}>7 Days</MenuItem>
+				<MenuItem value={30}>30 Days</MenuItem>
+				<MenuItem value={60}>60 Days</MenuItem>
+				<MenuItem value={90}>90 Days</MenuItem>
+				<MenuItem value={120}>120 Days</MenuItem>
+				<MenuItem value={365}>1 Year</MenuItem>
+			</Select>
+		</div>
+	);
+};
 
-export default SelectCoins
+export default SelectCoins;
