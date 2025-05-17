cd84696adc093af5e6529b4ff6200abfd08e0548
Ankush Kailas Dhavale <dhavaleankush7@gmail.com>
2025-05-17 12:02:10 +0530

    Build Responsiveness

--- src/components/Dashboard/List/List.jsx
+++ src/components/Dashboard/List/List.jsx
@@ -8,43 +8,43 @@ const List = ({ coin }) => {
 	return (
 		<tr className='list-row'>
 			<Tooltip title='Coin Logo' placement='bottom-end'>
-				<td className='td-image'>
-					<img src={coin?.image} className='coin-logo' />
+				<td className='td-image td-img-res'>
+					<img src={coin?.image} className='coin-logo logo-res' />
 				</td>
 			</Tooltip>
 			<Tooltip title='Coin Info' placement='bottom-start'>
 				<td>
-					<div className='name-col'>
-						<p className='coin-symbol'>{coin?.symbol}</p>
-						<p className='coin-name'>{coin?.name?.slice(1, 17)}</p>
+					<div className='name-col name-col-res'>
+						<p className='coin-symbol coin-symbol-res'>{coin?.symbol}</p>
+						<p className='coin-name coin-name-res'>{coin?.name?.slice(1, 17)}</p>
 					</div>
 				</td>
 			</Tooltip>
 			<Tooltip title='Price Change in 24H' placement='bottom-end'>
 				{coin?.price_change_percentage_24h > 0 ? (
-					<td className='chip-flex'>
-						<div className='price-chip'>
+					<td className='chip-flex chip-flex-res'>
+						<div className='price-chip price-chip-res'>
 							{coin.price_change_percentage_24h?.toFixed(2) + "%"}
 						</div>
-						<div className='icon-chip'>
+						<div className='icon-chip icon-chip-res'>
 							<TrendingUpRoundedIcon />
 						</div>
 					</td>
 				) : (
-					<td className='chip-flex'>
-						<div className='price-chip price-chip-red'>
+					<td className='chip-flex chip-flex-res'>
+						<div className='price-chip price-chip-red price-chip-res'>
 							{coin?.price_change_percentage_24h?.toFixed(2) + "%"}
 						</div>
-						<div className='icon-chip chip-red'>
+						<div className='icon-chip chip-red icon-chip-res'>
 							<TrendingUpRoundedIcon />
 						</div>
 					</td>
 				)}
 			</Tooltip>
 			<Tooltip title='Coin Price' placement='bottom-end'>
-				<td className='coin-info'>
-					<h3
-						className='coin-price td-center-right'
+				<td className='coin-info coin-info-res'>
+					<h1
+						className='coin-price td-center-right coin-price-res'
 						style={{
 							color:
 							coin?.price_change_percentage_24h > 0
@@ -53,26 +53,26 @@ const List = ({ coin }) => {
 						}}
 					>
 						${coin?.current_price?.toLocaleString()}
-					</h3>
+					</h1>
 				</td>
 			</Tooltip>
 			<Tooltip title='Total Volume' placement='bottom-end'>
-				<td>
+				<td className="total-volume-res">
 					<p className='total-volume td-align-right'>
 						{coin?.total_volume?.toLocaleString()}
 					</p>
 				</td>
 			</Tooltip>
 			<Tooltip title='Market Cap' placement='bottom-end'>
-				<td className='desktop-td-mkt'>
+				<td className='desktop-td-mkt market-cap-res'>
 					<p className='total-cap td-align-right'>
 						${coin?.market_cap?.toLocaleString()}
 					</p>
 				</td>
 			</Tooltip>
 			<Tooltip title='Market Cap' placement='bottom-end'>
-				<td className='mobile-td-mkt'>
-					<p className='total-cap td-align-right'>
+				<td className='mobile-td-mkt '>
+					<p className='total-cap td-align-right total-cap-res'>
 						${convertNumbers(coin?.market_cap)}
 					</p>
 				</td>
