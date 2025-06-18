205808b18afe4dd57e214754669febbecb578463
Ankush Kailas Dhavale <dhavaleankush7@gmail.com>
2025-06-18 21:15:59 +0530

    Build some feature to help user

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
-	<>
+	<React.StrictMode>
 		<App />
-	</>
+	</React.StrictMode>
 );
